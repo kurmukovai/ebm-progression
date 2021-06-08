1ff9b2409bb36c88e9373ad6518d8e9385374153
kurmukovai <kurmukovai@gmail.com>
2021-06-08 00:35:18 -0500

    fix uniform probability generator

--- ebm/probability.py
+++ ebm/probability.py
@@ -14,24 +14,25 @@ def fit_distributions(X, y, normalize=False):
     
     avg = X[y==0, ...].mean(axis=0)
     std = X[y==0, ...].std(axis=0)
-    p_not_E = [norm(loc, s) for loc, s in zip(avg, std)]
+    p_not_e = [norm(loc, s) for loc, s in zip(avg, std)]
 
     left_min = X.min(axis=0)
-    p_E = [uniform(m1, m2) for m1, m2 in zip(left_min, avg)]
-    return np.array(p_E), np.array(p_not_E)
+    right_max = avg.copy()
+    p_e = [uniform(m1, m2-m1) for m1, m2 in zip(left_min, right_max)]
+    return p_e, p_not_e, left_min, right_max
 
 
 def log_distributions(X, y, normalize=False, eps=1e-6):
     """Precomute probabilities for all features."""
     X = np.array(X).astype(np.float64)
     y = np.array(y)
-    cdf_p_E, cdf_p_not_E = fit_distributions(X, y, normalize=normalize)
+    cdf_p_e, cdf_p_not_e, left_min, right_max = fit_distributions(X, y, normalize=normalize)
     
     n, m = X.shape
-    log_p_E, log_p_not_E = np.zeros_like(X), np.zeros_like(X)
+    log_p_e, log_p_not_e = np.zeros_like(X), np.zeros_like(X)
 
     for i in range(n):
         for j in range(m):
-            log_p_E[i,j] = np.log(1 - cdf_p_E[j].cdf(X[i, j])+eps)
-            log_p_not_E[i,j] = np.log(cdf_p_not_E[j].cdf(X[i, j])+eps)
-    return log_p_E, log_p_not_E
+            log_p_e[i,j] = np.log(1 - cdf_p_e[j].cdf(np.clip(X[i, j], left_min[j]+eps, right_max[j]-eps))+eps)
+            log_p_not_e[i,j] = np.log(cdf_p_not_e[j].cdf(X[i, j])+eps)
+    return log_p_e, log_p_not_e
